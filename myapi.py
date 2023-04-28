bd26d208ca1e5251e9c9b107540522d9a4e6a7da
Varad Prabhu <varadp763@gmail.com>
2023-04-29 01:18:12 +0530

    Tested encry & decryp functions

--- myapi.py
+++ myapi.py
@@ -14,7 +14,7 @@ def dataEncryption(data):
     token=Fernet(key).encrypt(bytes(data,'utf-8'))
     return [token.decode('utf-8'),key.decode('utf-8')]
 
-def dataDecrption(key,token):
+def dataDecryption(key,token):
     f=Fernet(bytes(key,'utf-8'))
     data=f.decrypt(bytes(token,'utf-8'))
     return data.decode('utf-8')
@@ -42,8 +42,14 @@ def home():
 @app.post("/encrypt-data")
 def encryptPlayload(data : str):
      keydata=dataEncryption(data)
-     crypto={'key':keydata[0],'data':keydata[1]}
+     crypto={'key':keydata[1],'data':keydata[0]}
      return crypto
+
+@app.post("/decrypt-data")
+def decrpytoPlayload(data:Payload):
+    token=dataDecryption(data.key,data.data)
+    crypto={'message':token}
+    return crypto     
 
 if __name__ == '__main__':
     uvicorn.run(app, port=80, host='0.0.0.0')
